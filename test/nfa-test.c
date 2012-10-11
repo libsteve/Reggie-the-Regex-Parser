ed3cf48986153aa96d467f51f90dd58d84565db2
Steven Brunwasser <sbrunwasser@gmail.com>
2012-10-11 16:13:41 -0400

    yeah... i got bored with the nfa tests. these are a little too easy, so I'm just assuming they work

---
+++ test/nfa-test.c
@@ -0,0 +1,33 @@
+#include "nfa-test.h"
+
+//
+// i got lazy about testing this... most of it is REALLY easy...
+//
+
+////
+// test 
+//
+// result test_() {
+// 	int passed = 0;
+// 	char* description = "";
+//
+//	
+//
+// 	passed = ;
+//
+//	
+//
+// 	return (result){passed, description};
+// }
+
+////
+// main function and test list definition
+
+static tests TESTS = {
+	0
+};
+
+int main(int argc, char** argv) {
+	run_tests(TESTS);
+	return 0;
+}
---
+++ test/nfa-test.h
@@ -0,0 +1,7 @@
+#ifndef NFA_TEST_H
+#define NFA_TEST_H
+
+#include "tester.h"
+#include "../src/nfa.h"
+
+#endif
