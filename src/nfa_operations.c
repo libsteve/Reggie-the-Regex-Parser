62b0d57780210b79619f192b68d390746986969c
Steven Brunwasser <sbrunwasser@gmail.com>
2013-06-26 23:15:42 -0700

    updated nfa_operations to use nfa_mold

--- src/nfa_operations.c
+++ src/nfa_operations.c
@@ -1,110 +1,112 @@
 #include "nfa_operations.h"
 #include "list.h"
-
-// private
-// for list nfa_getTerminalStates(NFA)
-// perform this function on every state in an NFA
-// if the state is a terminal state, add it to the list of terminal states
-void nfa_getTerminalStates_foreachWithState_func(void* state, void* value) {
-	State s = (State)value;
-	list terminalStates = (list)state;
-
-	if (s->isTerminalState)
-		list_push(terminalStates, s);
-}
-
-// private
-// get the terminal states from an NFA
-list nfa_getTerminalStates(NFA nfa) {
-	list l = list_create();
-	list_foreachWithState(nfa->states, &nfa_getTerminalStates_foreachWithState_func, l);
-	return l;
-}
-
-// private
-// creates an epsilon transition between a start state and a destination state
-// makes the start state non-terminal
-// persistantState - the state for all transitions to lead to
-// variableState - the state for transitions to originate from
-// this conforms to the foreachWithState_func
-void __state_makeEpsilonTransition(void* persistantState, void* variableState) {
-	// make an epsilon transition from the terminalState to b's initial
-	state_addTransition((State)variableState, "", (State)persistantState);
-
-	// this state is no longer a terminal state
-	state_makeNonTerminal((State)variableState);
-}
-
-// private
-// adds a value to each id for all the states in the given list
-void states_addToID(list states, unsigned int i) {
-	FOREACH(it, states) {
-		State s = VALUE(it);
-		s->id += i;
-	}
-}
+#include "nfa_create.h"
+#include "nfa_copy.h"
 
 NFA nfa_CONCAT(NFA a, NFA b) {	
-	list terminalStates = nfa_getTerminalStates(a);
-	list_foreachWithState(terminalStates, &__state_makeEpsilonTransition, nfa_initialState(b));
+	NFAMold mold_a = nfa_decompile(a);
+	NFAMold mold_b = nfa_decompile(b);
 
-	states_addToID(b->states, list_len(a->states));
-	list allStates = list_merge(a->states, b->states);
-	State initialState = nfa_initialState(a);
+	FOREACH(it, mold_b->states) {
+		StateMold sm = VALUE(it);
+		sm->id += a->state_count;
+	}
+	FOREACH(it, mold_b->transitions) {
+		TransitionMold tm = VALUE(it);
+		tm->source += a->state_count;
+		tm->dest += a->state_count;
+	}
 
-	a->states = list_create(); // we need a list int the NFA to safely destroy
-	b->states = list_create(); // we need a list int the NFA to safely destroy
-	nfa_destroy(a);
-	nfa_destroy(b);
+	list states_to_transition = list_create();
+	unsigned int mold_b_initialState = mold_a->state_count;
+	FOREACH(it, mold_a->states) {
+		StateMold sm = VALUE(it);
+		if (sm->isTerminal) {
+			list_push(states_to_transition);
+		}
+	}
 
-	NFA nfa = nfa_create();
-	list_destroy(nfa->states); // destroy what is given
+	mold_a->state_count += mold_b->state_count;
 
-	nfa->states = allStates; // use what we have
-	nfa_setInitialState(nfa, initialState); // use what we have
+	mold_a->states = list_merge(mold_a->states, mold_b->states);
+	mold_a->transitions = list_merge(mold_a->transitions, mold_b->transitions);
 
-	list_destroy(terminalStates);
+	// create dummy lists for deallocation
+	mold_b->states = list_create();
+	mold_b->states = list_create();
+	nfa_mold_destroy(mold_b);
 
-	return nfa;
+	FOREACH(it, states_to_transition) {
+		StateMold sm = VALUE(it);
+		nfa_mold_addTransition(mold_a, sm->id, "", mold_b_initialState);
+		sm->isTerminal = 0;
+	}
+
+	NFA result = nfa_mold_compile(mold_a);
+	nfa_mold_destroy(mold_a);
+	return result;
 }
 
 NFA nfa_UNION(NFA a, NFA b) {
-	NFA nfa = nfa_create();
+	NFAMold mold_a = nfa_decompile(a);
+	NFAMold mold_b = nfa_decompile(b);
+	NFAMold mold_result = nfa_mold_create();
 
-	state_addTransition(nfa_initialState(nfa), "", nfa_initialState(a));
-	state_addTransition(nfa_initialState(nfa), "", nfa_initialState(b));
+	unsigned int mold_a_initial = 1;
+	FOREACH(it, mold_a->states) {
+		StateMold sm = VALUE(it);
+		sm->id += 1;
+	}
+	mold_result->states = list_merge(mold_result->states, mold_a->states);
+	mold_result->transitions = list_merge(mold_result->transitions, mold_a->transitions);
+	mold_result->state_count += mold_a->state_count;
 
-	states_addToID(a->states, 1);
-	states_addToID(b->states, ((unsigned int)list_len(a->states)) + (unsigned int)1);
+	// create dummy lists for deallocation
+	mold_a->states = list_create();
+	mold_a->states = list_create();
+	nfa_mold_destroy(mold_a);
 
-	nfa->states = list_merge(list_merge(nfa->states, a->states), b->states);
+	unsigned int mold_b_initial = mold_result->state_count;
+	FOREACH(it, mold_b->states) {
+		StateMold sm = VALUE(it);
+		sm->id += mold_result->state_count;
+	}
+	mold_result->states = list_merge(mold_result->states, mold_b->states);
+	mold_result->transitions = list_merge(mold_result->transitions, mold_b->transitions);
+	mold_result->state_count += mold_b->state_count;
 
-	a->states = list_create(); // we need a list int the NFA to safely destroy
-	b->states = list_create(); // we need a list int the NFA to safely destroy
-	nfa_destroy(a);
-	nfa_destroy(b);
+	// create dummy lists for deallocation
+	mold_b->states = list_create();
+	mold_b->states = list_create();
+	nfa_mold_destroy(mold_b);
 
-	return nfa;
+	nfa_mold_addTransition(mold_result, 0, "", mold_a_initial);
+	nfa_mold_addTransition(mold_result, 0, "", mold_b_initial);
+
+	NFA result = nfa_mold_compile(mold_result);
+	nfa_mold_destroy(mold_result);
+	return result;
 }
 
 NFA nfa_KLEENE(NFA a) {
-	NFA nfa = nfa_create();
-	state_makeTerminal(nfa_initialState(nfa));
+	NFAMold copy_mold = nfa_decompile(a);
 
-	list terminalStates = nfa_getTerminalStates(a);
-	list_foreachWithState(terminalStates, &__state_makeEpsilonTransition, nfa_initialState(nfa));
+	FOREACH(it, copy_mold->states) {
+		StateMold sm = VALUE(it);
+		if (sm->isTerminal) {
+			nfa_mold_addTransition(copy_mold, sm->id, "", 0);
+			sm->isTerminal = 0;
+		}
+	}
 
-	state_addTransition(nfa_initialState(nfa), "", nfa_initialState(a));
+	NFA result = nfa_mold_compile(copy_mold);
+	nfa_mold_destroy(copy_mold);
+	return result;
+}
 
-	states_addToID(a->states, 1);
-
-	list_rpush(a->states, list_pop(nfa->states));
-
-	list temp = nfa->states;
-	nfa->states = a->states;
-	a->states = temp;
-
-	nfa_destroy(a);
-
-	return nfa;
+NFA nfa_PLUS(NFA a) {
+	NFA kleene = nfa_KLEENE(a);
+	NFA result = nfa_UNION(a, kleene);
+	nfa_destroy(kleene);
+	return result;
 }
--- src/nfa_operations.h
+++ src/nfa_operations.h
@@ -4,15 +4,15 @@
 #include "nfa.h"
 
 // take two NFAs and concatenate them together into a new NFA.
-// distroys the two given NFAs.
 NFA nfa_CONCAT(NFA a, NFA b);
 
 // take two NFAs and union them together into a new NFA.
-// distroys the two given NFAs.
 NFA nfa_UNION(NFA a, NFA b);
 
 // take an NFA and kleene star it into a new NFA.
-// distroys the two given NFAs.
 NFA nfa_KLEENE(NFA a);
+
+// take an NFA and concatenate it with it's kleene star
+NFA nfa_PLUS(NFA a);
 
 #endif
