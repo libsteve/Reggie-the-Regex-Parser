557e94d3d820f3afc9e489c0010352fe2911d5f5
Steven Brunwasser <sbrunwasser@gmail.com>
2013-06-26 23:44:25 -0700

    fixed some bugs

--- src/nfa_operations.c
+++ src/nfa_operations.c
@@ -58,6 +58,11 @@ NFA nfa_UNION(NFA a, NFA b) {
 		StateMold sm = VALUE(it);
 		sm->id += 1;
 	}
+	FOREACH(it, mold_a->transitions) {
+		TransitionMold tm = VALUE(it);
+		tm->source += 1;
+		tm->dest += 1;
+	}
 	mold_result->states = list_merge(mold_result->states, mold_a->states);
 	mold_result->transitions = list_merge(mold_result->transitions, mold_a->transitions);
 	mold_result->state_count += mold_a->state_count;
@@ -71,6 +76,11 @@ NFA nfa_UNION(NFA a, NFA b) {
 	FOREACH(it, mold_b->states) {
 		StateMold sm = VALUE(it);
 		sm->id += mold_result->state_count;
+	}
+	FOREACH(it, mold_b->transitions) {
+		TransitionMold tm = VALUE(it);
+		tm->source += mold_result->state_count;
+		tm->dest += mold_result->state_count;
 	}
 	mold_result->states = list_merge(mold_result->states, mold_b->states);
 	mold_result->transitions = list_merge(mold_result->transitions, mold_b->transitions);
@@ -99,6 +109,7 @@ NFA nfa_KLEENE(NFA a) {
 			sm->isTerminal = 0;
 		}
 	}
+	nfa_mold_makeStateTerminal(copy_mold, 0);
 
 	NFA result = nfa_mold_compile(copy_mold);
 	nfa_mold_destroy(copy_mold);
