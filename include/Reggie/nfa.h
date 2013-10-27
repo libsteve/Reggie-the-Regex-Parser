c000ca8d784e212b530eb9eca99adc7287bcf2f3
Steven Brunwasser <sbrunwasser@gmail.com>
2013-10-27 00:58:29 -0400

    updated nfa to use generic automata removed nfa_create, since that functionality was added natively

--- include/Reggie/nfa.h
+++ include/Reggie/nfa.h
@@ -1,91 +1,75 @@
 #ifndef NFA_H
 #define NFA_H
 
-#include "Collection/list.h"
+#include <CReggie/automata.h>
 
-// a transition to a state
-typedef struct transition {
-	char* transition_string; // a string to transition through when encountered
+// a transition for an nfa
+typedef struct nfa_transition {
+	struct transition transition;
+	char *transition_string; // a string to transition through when encountered
 							 // this must be an allocated string
-	struct state* source; // the source state for the transition
-	struct state* dest; // the destination state for the transition
-} *Transition;
+} *NFATransition;
 
 // a state in an nfa
-typedef struct state {
-	int isTerminalState; // 1 if the state is terminal, 0 otherwise
-	unsigned int id; // an id number for the state
-	char* name; // the name of the state
-				// this must be an allocated string
-	list transitions; // a list of transitions that originate from this state
-} *State;
+typedef struct nfa_state {
+	struct state state;
+} *NFAState;
 
 // a non-deterministic finite automata
 typedef struct nfa {
-	struct state* initialState; // the initial state for the nfa
-	list states; // a list of states that exist within the nfa
+	struct automata automata;
 } *NFA;
 
 // create a new nfa object
-// all nfa's begin with an initial state named "q0"
-// "q0" is initially a non-terminal state with no transitions
+// all NFAs are created with an initial state
 NFA nfa_create();
 
 // destroy an nfa object and all states within
 // use this instead of free
 void nfa_destroy(NFA nfa);
 
-// get the initial state of the NFA
-State nfa_initialState(NFA nfa);
 
-// remove the current initail state from the NFA 
-// and replace it with the given state
-// any transitions that might exist to this state will segfault
-void nfa_setInitialState(NFA nfa, State s);
+//////
+// Initialization Functions
+
+NFA nfa_initialize(NFA nfa, automata_destroy destroy);
+void nfa_uninitialize(automata a);
+
+NFAState nfa_state_initialize(NFAState s, bool isTerminal, state_destroy destroy);
+void nfa_state_uninitialize(state s);
+
+NFATransition nfa_transition_initialize(NFATransition t, state src, state dest, char *transition_string, transition_destroy destroy);
+void nfa_transition_uninitialize(transition t);
+
+int nfa_transition_func(const struct automata *a, const struct transition *t, const char *input); 
+
+
+//////
+// NFA Interaction Functions
+
+// get the initial state of the NFA
+state_id nfa_initialState(NFA nfa);
 
 // add a state to the NFA's state list
-void nfa_addState(NFA nfa, State s);
+// returns -1 if failure
+state_id nfa_addState(NFA nfa);
+#define nfa_makeState(nfa) nfa_addState(nfa)
+void nfa_removeState(NFA nfa, state_id sid);
 
-// create a new state object
-State state_create();
+bool nfa_state_isTerminal(NFA nfa, state_id sid);
+void nfa_state_setIsTerminal(NFA nfa, state_id sid, bool isTerminal);
+#define nfa_state_makeTerminal(nfa, sid) 	nfa_state_setIsTerminal(nfa, sid, true)
+#define nfa_state_makeNonTerminal(nfa, sid) nfa_state_setIsTerminal(nfa, sid, false)
 
-// give an ID to a state
-void state_setID(State s, unsigned int id);
+// returns -1 if failure
+transition_id nfa_addTransition(NFA nfa, state_id sid1, state_id sid2, char *transition_string);
+void nfa_removeTransition(NFA nfa, transition_id tid);
 
-// get the id of the state
-unsigned int state_getID(State s);
-
-// give a name to a state
-// you free the name string if dynamically allcoated
-void state_setName(State s, char* name);
-
-// get the name string of the state
-char *state_getName(State s);
-
-// is the state a terminal state?
-#define state_isTerminal(s) (s->isTerminalState)
-
-// set a state as being a terminal state
-void state_makeTerminal(State s);
-
-// set a state as being a non-terminal state
-void state_makeNonTerminal(State s);
-
-// destroy a state and all transitions from it
-// use this instead of free
-void state_destroy(State s);
-
-// add a transition from one state to another through the transition string
-// you free the transition_string string if dynamically allocated
-void state_addTransition(State s, char* transition_string, State dest);
-
-// remove a transition from one state to another through the transition string
-void state_removeTransition(State s, char* transition_string, State dest);
 
 void nfa_print(NFA nfa);
 
-void state_print(State s);
+void state_print(NFAState s);
 
-void transition_print(Transition t);
+void transition_print(NFATransition t);
 
 #endif
