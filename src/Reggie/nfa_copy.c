c44b7e3646b454718b0eb893a0632f603be799c9
Steven Brunwasser <sbrunwasser@gmail.com>
2013-10-27 03:31:55 -0400

    implemented copy with new nfa

--- src/Reggie/nfa_copy.c
+++ src/Reggie/nfa_copy.c
@@ -1,51 +1,41 @@
-#include "Collection/list_sort.h"
-#include "Reggie/nfa_copy.h"
-#include "Collection/strings.h"
+#include <Reggie/nfa_copy.h>
+#include <Collection/list_sort.h>
 
-int state_sorter(State a, State b) {
-	if (state_getID(a) <= state_getID(b))
+int state_sorter(state a, state b) {
+	if (a->id <= b->id)
 		return 1;
 	return 0;
 }
 
-// generate an nfa mold from a given nfa
-NFAMold nfa_mold_createFromNFA(NFA nfa) {
-	NFAMold mold = nfa_mold_create();
-	// copy the initial state
-	// for each state in the nfa that isn't the initial state, copy it
-	// make copies of each transition and attach them to the correct copies states
-	State initial = nfa_initialState(nfa);
-	list sortedStates = list_sort(nfa->states, (sort_fn)&state_sorter);
-	if (state_isTerminal(initial)) {
-		nfa_mold_makeStateTerminal(mold, nfa_mold_initialState(mold));
-	}
+// create a copy of an nfa from a given nfa
+NFA nfa_copy(NFA nfa) {
+	NFA copy = nfa_create();
+	list_destroy(nfa->automata.states);
+	nfa->automata.states = list_create();
+	nfa->automata.next_state_id = 0;
+
+	// for each state in the nfa, copy it
+	// make copies of each transition and attach them to the correct copied states
+	list sortedStates = list_sort(nfa->automata.states, (sort_fn)&state_sorter);
 
 	FOREACH(it, sortedStates) {
-		State s = VALUE(it);
+		state s = VALUE(it);
 		if (s != initial) {
-			unsigned int i = nfa_mold_addState(mold);
-			if (state_isTerminal(s))
-				nfa_mold_makeStateTerminal(mold, i);
+			unsigned int i = nfa_addState(copy);
+			if (s->isTerminal) nfa_state_makeTerminal(copy, s);
 		}
 	}
 
 	FOREACH(it, sortedStates) {
-		State s = VALUE(it);
+		state s = VALUE(it);
 		FOREACH(t_it, s->transitions) {
-			Transition t = VALUE(t_it);
-			State d = t->dest;
-			nfa_mold_addTransition(mold, state_getID(s), t->transition_string, state_getID(d));
+			transition t = VALUE(t_it);
+			NFATransition nfat = container_of(t, struct nfa_transition, transition);
+			state d = t->dest;
+			nfa_addTransition(mold, s->id, d->id, nfat->transition_string);
 		}
 	}
 
 	list_destroy(sortedStates);
-	return mold;
-}
-
-// create a copy of an nfa from a given nfa
-NFA nfa_copy(NFA nfa) {
-	NFAMold mold = nfa_mold_createFromNFA(nfa);
-	NFA copy = nfa_createFromMold(mold);
-	nfa_mold_destroy(mold);
 	return copy;
 }
