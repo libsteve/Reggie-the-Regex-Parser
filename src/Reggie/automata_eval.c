6c671dccd87ecf8956e1c94ca43fb12248a8157c
Steven Brunwasser <sbrunwasser@gmail.com>
2013-12-13 06:35:32 -0500

    undoing a transition now calls rewind() on the evalstream

--- src/Reggie/automata_eval.c
+++ src/Reggie/automata_eval.c
@@ -29,13 +29,22 @@ bool state_eval(automata a, state s, evalstream *input) {
 bool transition_eval(automata a, transition t, evalstream *input) {
 	int result = t->func(a, t, input);
 	if (result != -1) {
-		return state_eval(a, t->dst, input->fastforward(input, result));
+		input = input->fastforward(input, result);
+		bool success = state_eval(a, t->dst, input);
+		if (success) {
+			return success;
+		} else {
+			input->rewind(input, result);
+			return false;
+		}
 	}
 	return false;
 }
 
 //////
 // partial parsing evaluations of automata
+
+#define failure -1
 
 int automata_parsing_eval(automata a, evalstream *input) {
 	FOREACH(it, a->states) {
@@ -44,7 +53,7 @@ int automata_parsing_eval(automata a, evalstream *input) {
 			return state_parsing_eval(a, s, input);
 		}
 	}
-	return -1;
+	return failure;
 }
 
 int state_parsing_eval(automata a, state s, evalstream *input) {
@@ -54,17 +63,24 @@ int state_parsing_eval(automata a, state s, evalstream *input) {
 	FOREACH(it, s->transitions) {
 		transition t = VALUE(it);
 		int result = transition_parsing_eval(a, t, input);
-		if (result != -1) {
+		if (result != failure) {
 			return result;
 		}
 	}
-	return -1;
+	return failure;
 }
 
 int transition_parsing_eval(automata a, transition t, evalstream *input) {
 	int result = t->func(a, t, input);
-	if (result != -1) {
-		return state_parsing_eval(a, t->dst, input->fastforward(input, result));
+	if (result != failure) {
+		input = input->fastforward(input, result);
+		int success = state_parsing_eval(a, t->dst, input);
+		if (success != -1) {
+			return success + result;
+		} else {
+			input->rewind(input, result);
+			return failure;
+		}
 	}
-	return -1;
+	return failure;
 }
