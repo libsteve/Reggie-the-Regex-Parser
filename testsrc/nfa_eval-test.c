68059037ec892cea2084b6ae34d4e000a6ffcb33
Steven Brunwasser <sbrunwasser@gmail.com>
2013-12-13 03:23:32 -0500

    fixed problems with nfa_eval-test

--- testsrc/nfa_eval-test.c
+++ testsrc/nfa_eval-test.c
@@ -6,18 +6,18 @@
 // set up an NFA for the regex aba
 
 NFA aba_nfa() {
-	NFAMold mold = nfa_create();
-	state_id q0 = nfa_initialState(mold);
-	state_id q1 = nfa_addState(mold);
-	state_id q2 = nfa_addState(mold);
-	state_id q3 = nfa_addState(mold);
-	nfa_state_makeTerminal(mold, q3);
+	NFA n = nfa_create();
+	state_id q0 = nfa_initialState(n);
+	state_id q1 = nfa_addState(n);
+	state_id q2 = nfa_addState(n);
+	state_id q3 = nfa_addState(n);
+	nfa_state_makeTerminal(n, q3);
 
-	nfa_addTransition(mold, q0, q1, "a");
-	nfa_addTransition(mold, q1, q2, "b");
-	nfa_addTransition(mold, q2, q3, "a");
+	nfa_addTransition(n, q0, q1, "a");
+	nfa_addTransition(n, q1, q2, "b");
+	nfa_addTransition(n, q2, q3, "a");
 
-	return nfa;
+	return n;
 }
 
 ////
@@ -57,9 +57,9 @@ result test_state_eval_EmptyString_FailingNonTerminal() {
 
 	NFA n = nfa_create();
 
-	passed = is_false(state_eval(s, ""));
+	passed = is_false(nfa_eval(n, ""));
 
-	state_destroy(s);
+	nfa_destroy(n);
 
 	return (result){passed, description};
 }
@@ -178,11 +178,11 @@ result test_nfa_eval_ABA_PASSING() {
 	int passed = 0;
 	char* description = "nfa_eval(NFA, char*) : aba passing CASE";
 
-	NFA nfa = aba_nfa();
+	NFA n = aba_nfa();
 
-	passed = is_true(nfa_eval(nfa, "aba"));
+	passed = is_true(nfa_eval(n, "aba"));
 
-	nfa_destroy(nfa);
+	nfa_destroy(n);
 
 	return (result){passed, description};
 }
@@ -191,24 +191,24 @@ result test_nfa_eval_ABA_PASSINGWithEpsilons() {
 	int passed = 0;
 	char* description = "nfa_eval(NFA, char*) : aba passing with epsilons CASE";
 
-	NFA nfa = nfa_create();
+	NFA n = nfa_create();
 	state_id states[8];
-	states[0] = nfa_initialState(nfa);
+	states[0] = nfa_initialState(n);
 	for (int i = 1; i < 8; i++) {
-		states[i] = nfa_addState(nfa);
+		states[i] = nfa_addState(n);
 	}
 
-	nfa_addTransition(nfa, states[0], states[1], "");
-	nfa_addTransition(nfa, states[1], states[2], "");
-	nfa_addTransition(nfa, states[2], states[3], "");
-	nfa_addTransition(nfa, states[3], states[4], "a");
-	nfa_addTransition(nfa, states[4], states[5], "b");
-	nfa_addTransition(nfa, states[5], states[6], "a");
-	nfa_state_makeTerminal(nfa, states[6]);
+	nfa_addTransition(n, states[0], states[1], "");
+	nfa_addTransition(n, states[1], states[2], "");
+	nfa_addTransition(n, states[2], states[3], "");
+	nfa_addTransition(n, states[3], states[4], "a");
+	nfa_addTransition(n, states[4], states[5], "b");
+	nfa_addTransition(n, states[5], states[6], "a");
+	nfa_state_makeTerminal(n, states[6]);
 
-	passed = is_true(nfa_eval(nfa, "aba"));
+	passed = is_true(nfa_eval(n, "aba"));
 
-	nfa_destroy(nfa);
+	nfa_destroy(n);
 
 	return (result){passed, description};
 }
@@ -217,23 +217,23 @@ result test_nfa_eval_aUb_PASSINGWithEpsilons() {
 	int passed = 0;
 	char* description = "nfa_eval(NFA, char*) : a|b passing with epsilons CASE";
 
-	NFA mold = nfa_create();
-	state_id q0 = nfa_initialState(mold);
-	state_id q1 = nfa_addState(mold);
-	state_id q2 = nfa_addState(mold);
-	state_id q3 = nfa_addState(mold);
-	state_id q4 = nfa_addState(mold);
+	NFA n = nfa_create();
+	state_id q0 = nfa_initialState(n);
+	state_id q1 = nfa_addState(n);
+	state_id q2 = nfa_addState(n);
+	state_id q3 = nfa_addState(n);
+	state_id q4 = nfa_addState(n);
 
-	nfa_addTransition(mold, q0, q1, "");
-	nfa_addTransition(mold, q0, q2, "");
-	nfa_addTransition(mold, q1, q3, "a");
-	nfa_addTransition(mold, q2, q4, "b");
-	nfa_state_makeTerminal(mold, q3);
-	nfa_state_makeTerminal(mold, q4);
+	nfa_addTransition(n, q0, q1, "");
+	nfa_addTransition(n, q0, q2, "");
+	nfa_addTransition(n, q1, q3, "a");
+	nfa_addTransition(n, q2, q4, "b");
+	nfa_state_makeTerminal(n, q3);
+	nfa_state_makeTerminal(n, q4);
 
-	passed = is_true(nfa_eval(nfa, "a")) && is_true(nfa_eval(nfa, "b"));
+	passed = is_true(nfa_eval(n, "a")) && is_true(nfa_eval(n, "b"));
 
-	nfa_destroy(nfa);
+	nfa_destroy(n);
 
 	return (result){passed, description};
 }
@@ -242,11 +242,11 @@ result test_nfa_eval_ABA_Failing() {
 	int passed = 0;
 	char* description = "nfa_eval(NFA, char*) : aba failing CASE";
 
-	NFA nfa = aba_nfa();
+	NFA n = aba_nfa();
 
-	passed = is_false(nfa_eval(nfa, "aaa"));
+	passed = is_false(nfa_eval(n, "aaa"));
 
-	nfa_destroy(nfa);
+	nfa_destroy(n);
 
 	return (result){passed, description};
 }
@@ -277,3 +277,4 @@ static tests TESTS = {
 int main(int argc, char** argv) {
 	return run_tests(TESTS);
 }
+
