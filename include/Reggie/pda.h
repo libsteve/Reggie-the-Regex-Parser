f62df30fb792e5648051983c12138de1cb9caa33
Steven Brunwasser <sbrunwasser@gmail.com>
2013-12-20 01:59:02 -0500

    began to refactor pda headers

--- include/Reggie/pda.h
+++ include/Reggie/pda.h
@@ -1,39 +1,43 @@
-#ifndef PDA_H
-#define PDA_H
+#ifndef REGGIE_PDA_H
+#define REGGIE_PDA_H
 
-#include "Collection/list.h"
+#include <Collection/list.h>
+#include <Reggie/automata.h>
+#include <Reggie/nfa.h>
 
 struct pda;
 struct pda_state;
 struct pda_transition;
+struct pda_evalstream;
 
-// a function that a transition can use to determine whether or not the transition should be followed
-// the function takes a PDA, the IDs of the source and destination states, 
-// the input string, and any arbitrary data to be utilized by the function implementation.
-// the function should return the length of an accepting string,
-// 0 if the empty string is accepted or if the function passes due to data on the PDA's stack,
-// and -1 if the transition should not be accepted.
-typedef int (*pda_transition_fn)(struct pda *pda, int src, int dest, char *input, void *data);
+typedef void (*pda_transition_apply_func)(const struct pda *pda, const struct pda_transition *t, const pda_evalstream *input, );
+typedef void (*pda_transition_revoke_func)(const struct pda *pda, const struct pda_transition *t, const pda_evalstream *input, );
 
 // a structure to represent a state within a PDA
 typedef struct pda_state {
-	unsigned int id;	// an identifier number for the state
-	int isTerminal;		// a boolean flag to dentoe that the state is termianl
-	list transitions;	// a list of transitions from this state
+	struct state state;
 } *PDAState;
 
 // a structure to represent a PDA transition
 typedef struct pda_transition {
-	unsigned int id;			// an identifier number for the trnasition
-	unsigned int source;		// the source state ID
-	unsigned int dest;			// the destination state ID
-	pda_transition_fn function;	// the PDA transition function
+	struct state state;
+    enum pda_transition_type {
+        PDA_TRANSITION_TYPE_STRING,
+        PDA_TRANSITION_TYPE_NFA,
+        PDA_TRANSITION_TYPE_PDA
+    } type;
+    union {
+        char *string;   // an allocated, null-terminated transition string
+        NFA nfa;        // a copied NFA
+        PDA pda;        // a copied PDA
+    };
+	pda_transition_apply_func apply;    // apply this when perfroming the transition
+    pda_transition_revoke_func revoke;  // undo the application of this transiton
 } *PDATransition;
 
 // a structure to represent a push-down automata
 typedef struct pda {
-	list states;		// a list of states in the PDA
-	list transitions;	// a list of transitions between the states
+	struct automata automata;
 	list stack;			// a stack of data that can be used by transition functions
 } *PDA;
 
@@ -43,31 +47,48 @@ PDA pda_create();
 // destroy a PDA instance
 void pda_destroy(PDA pda);
 
-// add a state to the PDA
-int pda_addState(PDA pda);
 
-// returns the state struct for the PDA, or NULL if no state was found
-PDAState pda_getState(PDA pda, int state);
+//////
+// Initialization Functions
 
-// check if the given state is terminal
-int pda_state_isTerminal(PDA pda, int state);
+PDA pda_initialize(PDA pda, automata_destroy destroy);
+void pda_uninitialize(automata a);
 
-// make the given state terminal
-void pda_state_makeTerminal(PDA pda, int state);
+PDAState pda_state_initialize(PDAState s, bool isTerminal, state_destroy destroy);
+void pda_state_uninitialize(state s);
 
-// make the given state non-terminal
-void pda_state_makeNonTerminal(PDA pda, int state);
+PDATransition nfa_transition_initialize(PDATransition t, state src, state dest, char *transition_string, transition_destroy destroy);
+void pda_transition_uninitialize(transition t);
 
-// returns the transition struct for the PDA, or NULL if no transition was found
-PDATransition pda_getTransition(PDA pda, int transition);
+int pda_transition_func(const struct automata *a, const struct transition *t, const evalstream *input); 
 
-// add a transition between the given states over the transition function
-int pda_addTransition(PDA pda, int src, int dest, pda_transition_fn func);
+// evalstream instance for pda_transition_func
+struct pda_evalstream {
+	struct evalstream stream;
+	char *string;
+};
 
-// remove the given transition
-void pda_removeTransition(PDA pda, int transition);
 
-// get the stack for the PDA
+
+//////
+// PDA Interaction Functions
+
+// add a state to the PDA's list
+// returns -1 if failure
+state_id pda_addState(PDA pda);
+void pda_removeState(PDA pda, state_id sid);
+
+bool pda_state_isTerminal(PDA pda, state_id sid);
+void pda_state_setIsTerminal(PDA pda, state_id sid, bool isTerminal);
+#define pda_state_makeTerminal(pda, sid) 	pda_state_setIsTerminal(pda, sid, true)
+#define pda_state_makeNonTerminal(pda, sid) pda_state_setIsTerminal(pda, sid, false)
+
+transition_id pda_addStringTransition(PDA pda, state_id sid1, state_id sid2, char *transition_string);
+transition_id pda_addNFATransition(PDA pda, state_id sid1, state_id sid2, NFA nfa);
+transition_id pda_addPDATransition(PDA pda, state_id sid1, state_id sid2, PDA pda);
+void pda_removeTransition(PDA pda, transition_id tid);
+
 list pda_stack(PDA pda);
 
 #endif
+
--- include/Reggie/pda_eval.h
+++ include/Reggie/pda_eval.h
@@ -1,23 +1,15 @@
-#ifndef PDA_EVAL_H
-#define PDA_EVAL_H
+#ifndef REGGIE_PDA_EVAL_H
+#define REGGIE_PDA_EVAL_H
 
-#include "pda.h"
+#include <Reggie/pda.h>
 
-// evaluate the pda with the given input string, using the given data for the transitions
-// returns the length of some passing string
-int pda_eval(PDA pda, char* input, void *data);
+// evaluate the pda with the given input string
+// returns true if the input string matches the pda, false otherwise
+bool pda_eval(PDA pda, char* input);
 
-// evaluate the state with the given input string, using the given data for the transitions
-// recursively evaluates through transitions
-// returns the length of some passing string
-// returns -1 if instant pass without length
-// returns 0 if it fails the state
-int pda_state_eval(PDA pda, PDAState s, char* input, void *data);
-
-// follow and evaluate the transition with the given input string, using the given data for the transitions
-// remove the transition string from the input
-// evaluates destination state
-// returns the length of some passing string
-int pda_transition_eval(PDA pda, PDATransition t, char* input, void *data);
+// evaluate the pda with the given input string
+// returns -1 if failure or the length of the first successful substring match
+int pda_parsing_eval(PDA pda, char* input);
 
 #endif
+
